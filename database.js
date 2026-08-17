6615553b2dcbc35f5918d6f2bbd2ec6b183be633
enjoy-all-tiktok-vairal-video <rajame331@gmail.com>
2026-08-18 03:15:55 +0600

    Update database.js

--- database.js
+++ database.js
@@ -2,7 +2,7 @@
 const videos = {
 "EP_02": { 
         title: "Episode 2", 
-        image: "https://share.google/ahh5RI7lpnJQGR4al", 
+        image: "https://i.ibb.co.com/NgkghkCX/170fd511e56261750fc78840464e6697.jpg", 
         final: "https://youtu.be/1myGZh27h6w?si=ZXxvuT9mJ33GSAQq", 
         cat: "all", 
         date: "18 Aug 2026", 
@@ -10,7 +10,7 @@ const videos = {
     },
     "EP_01": { 
         title: "Episode 1", 
-        image: "https://share.google/P2O6xoRKUtwImInuE", 
+        image: "https://i.ibb.co.com/YFyDMtGs/images.jpg", 
         final: "https://youtu.be/n9nLTL9mhaU?si=uVAsmdL5zzQf9VrE", 
         cat: "all", 
         date: "18 Aug 2026", 
